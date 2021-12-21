cba51a2944248db992fe95267ccc41fbf8f74373
RecursionSheep <zy-chen17@mails.tsinghua.edu.cn>
2021-12-21 14:40:18 +0800

    fix bugs

--- testgen/classifier.cpp
+++ testgen/classifier.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 const int N = 50, flip = 10;
-const int M = 10000000;
 double p[N][2];
 int z[N], perm[N];
 double w[2];
@@ -22,9 +21,11 @@ int main(int argc, char **argv) {
     int testcnt = atoi(argv[1]);
     for (int test = 1; test <= testcnt; test ++) {
         ofstream out("classifier" + to_string(test) + ".in");
+	double M = 0.;
         for (int i = 0; i < N; i ++) {
             auto point = Gaussian();
             p[i][0] = point.first; p[i][1] = point.second;
+	    M = max(M, fabs(p[i][0]) + fabs(p[i][1]));
         }
         auto point = Gaussian();
         w[0] = point.first; w[1] = point.second;
@@ -39,7 +40,7 @@ int main(int argc, char **argv) {
         for (int i = 0; i < flip; i ++)
             z[i] = -z[i];
         out << N + 2 << endl;
-        
+        //cout << w[0] << ' ' << w[1] << endl;
         out << "target: min ";
         for (int i = 0; i < N; i ++) {
             if (i != 0) out << " + ";
@@ -47,10 +48,10 @@ int main(int argc, char **argv) {
         }
         out << " ;" << endl;
         for (int i = 0; i < N; i ++) {
-            out << "constr: " << z[i] * p[i][0] << " * x" << N << " + " << z[i] * p[i][1] << " * x" << N + 1 << " + " << M << " * x" << i << " >= 0.0001 ;" << endl;
+            out << "constr: " << z[i] * p[i][0] << " * x" << N << " + " << z[i] * p[i][1] << " * x" << N + 1 << " + " << M << " * x" << i << " >= 0.01 ;" << endl;
         }
-        out << "bound: -1 <= x" << N << " <= 1 ;" << endl;
-        out << "bound: -1 <= x" << N + 1 << " <= 1 ;" << endl;
+        out << "bound: -10 <= x" << N << " <= 10 ;" << endl;
+        out << "bound: -10 <= x" << N + 1 << " <= 10 ;" << endl;
         for (int i = 0; i < N; i ++) {
             out << "bound: 0 <= x" << i << " <= 1 ;" << endl;
             out << "int: x" << i << " ;" << endl;
